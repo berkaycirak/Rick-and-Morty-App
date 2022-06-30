be1d97b0a8546fb98861bef9ae13a977e08187d8
Berkay <berkaycirak_11@hotmail.com>
2022-07-01 00:39:39 +0300

    Logo is added

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import './Navbar.styles.scss';
-import backgroundImg from '../../../assets/logo.png';
+import logoImg from '../../../assets/logo.png';
 
 function Navbar() {
   return (
     <>
       <nav>
         <div className='logo'>
-          <img src={backgroundImg} alt='logo' />
+          <img src={logoImg} alt='logo' />
         </div>
       </nav>
     </>
